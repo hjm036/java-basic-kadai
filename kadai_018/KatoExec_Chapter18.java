672754141818d32e78f01fd59457a7e86d805ae8
hjm036 <runa.0421_eon@icloud.com>
2024-10-21 23:26:45 +0900

    修正分2

--- kadai_018/KatoExec_Chapter18.java
+++ kadai_018/KatoExec_Chapter18.java
@@ -4,17 +4,20 @@ public class KatoExec_Chapter18 {
 
 	public static void main(String[] args) {
 		
-		Kato_Chapter18 KatoTaro = new KatoTaro_Chapter18();
-		Kato_Chapter18 KatoIchiro = new KatoIchiro_Chapter18();
-		Kato_Chapter18 KatoHanako = new KatoHanako_Chapter18();
+		KatoTaro_Chapter18 KatoTaro = new KatoTaro_Chapter18();
+		KatoIchiro_Chapter18 KatoIchiro = new KatoIchiro_Chapter18();
+		KatoHanako_Chapter18 KatoHanako = new KatoHanako_Chapter18();
 		
 		
+		KatoTaro.setGivenName();
 		KatoTaro.execIntroduce ();
 		System.out.println("");
 		
+		KatoIchiro.setGivenName();
 		KatoIchiro.execIntroduce();
 		System.out.println("");
 		
+		KatoHanako.setGivenName();
 		KatoHanako.execIntroduce();
 		
 		

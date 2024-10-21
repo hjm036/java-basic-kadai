e61c8c204b9ff6661d2398cd79ac956c5b52eac8
hjm036 <runa.0421_eon@icloud.com>
2024-10-21 21:21:42 +0900

    修正分

--- kadai_018/Kato_Chapter18.java
+++ kadai_018/Kato_Chapter18.java
@@ -2,7 +2,7 @@ package kadai_018;
 
 	abstract public class Kato_Chapter18 {
 
-	public String familiyName = "加藤";
+	public String familyName = "加藤";
 	public String givenName;
 	public String address =  "東京都中野区○×";
 	
@@ -12,7 +12,7 @@ package kadai_018;
 	
 	public void commonIntoroduce() {
 		
-		System.out.println("名前は" + this.familiyName + this.givenName + "です");
+		System.out.println("名前は" + this.familyName + this.givenName + "です");
 		System.out.println("住所は" + this.address + "です");
 		
 	}
